55a21475512b05a0f793193bc234306731060ac5
aydnperihan <aydnperihanhatice@gmail.com>
2022-02-17 13:49:07 +0300

    Bina class oluşturuldu

--- BLL/Models/Bina.cs
+++ BLL/Models/Bina.cs
@@ -1,6 +1,7 @@
 ﻿using DAL;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -16,14 +17,42 @@ namespace BLL.Models
 
         Execute _execute = new Execute();
         List<SqlParameter> _params = new List<SqlParameter>();
-        //Actions
+        SQL.Bina _sql = new SQL.Bina();
+     
         public bool ekle()
         {
             bool _result = false;
             _params.Add(new SqlParameter("@BinaAdi", BinaAdi));
+            _params.Add(new SqlParameter("@tipid", tipid));
             string _hataMesaji = "";
             _result = _execute.execute(_sql.sqlEkle(), _params.ToArray(), false, ref _hataMesaji);
 
+            return _result;
+        }
+
+        public DataTable listele()
+        {
+            string _hataMesaji = "";
+            return _execute.executeDT(_sql.sqlListele(), null, false, ref _hataMesaji);
+        }
+        public bool güncelle()
+        {
+            bool _result = false;
+            _params.Add(new SqlParameter("@BinaAdi", BinaAdi));
+            _params.Add(new SqlParameter("@id", id));
+            _params.Add(new SqlParameter("@tipid", tipid));
+            string _hataMesaji = "";
+            _result = _execute.execute(_sql.sqlGüncelle(), _params.ToArray(), false, ref _hataMesaji);
+
+            return _result;
+        }
+
+        public bool Sil()
+        {
+            bool _result = false;
+            _params.Add(new SqlParameter("@id", id));
+            string _hataMesaji = "";
+            _result = _execute.execute(_sql.sqlSil(), _params.ToArray(), false, ref _hataMesaji);
             return _result;
         }
     }
---
+++ SQL/Bina.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL
+{
+    public class Bina
+    {
+        public string sqlEkle()
+        {
+            string _sql = "insert into Bina (BinaAdi,tipid) values (@BinaAdi,@tipid);";
+
+            return _sql;
+        }
+        public string sqlListele()
+        {
+            string _sql = "select * from Bina";
+
+            return _sql;
+        }
+        public string sqlGüncelle()
+        {
+            string _sql = "update Bina set BinaAdi=@BinaAdi,tipid=@tipid where id=@id;";
+
+            return _sql;
+        }
+        public string sqlSil()
+        {
+            string _sql = "delete from Bina where id=@id";
+
+            return _sql;
+        }
+    }
+}
