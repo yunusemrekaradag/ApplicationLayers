c4b236923af7fc5622fded5d946a65c73cb665df
ilaydakosar <ilaydasevdenur34@hotmail.com>
2022-02-17 10:58:37 +0300

    ogrenci

---
+++ BLL/Models/Ogrenci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Models
+{
+    public class Ogrenci
+    {
+        //Properties
+
+         public int id { get; set; }
+        public string OgrenciAdi { get; set; }
+         public string OgrenciSoyadi { get; set; }
+         public string OgrenciSinif { get; set; }
+
+        public int DersId { get; set; }
+
+        // Actions 
+
+        
+
+    }
+}
